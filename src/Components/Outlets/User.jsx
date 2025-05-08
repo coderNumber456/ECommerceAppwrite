e87d627f803a2d9cc5cadeac58705bb6d07ef93a
coderNumber456 <suf28021999@gmail.com>
2025-05-08 11:20:52 +0530

    test push

--- src/Components/Outlets/User.jsx
+++ src/Components/Outlets/User.jsx
@@ -7,7 +7,7 @@ function User(){
     return(
         <div className="h-96">
             <h1 className="w-56 text-3xl mx-auto my-44">
-                Coming Soon....
+                Coming Soon
             </h1>
         </div>
     )
